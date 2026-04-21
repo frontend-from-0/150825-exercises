7e555e15296f232d8ba642d0e237fd816db4d5d4
ABID IHSAN <ihsanabid1995@gmail.com>
2026-04-21 12:17:02 +0300

    Lesson 23- Add initial implementation of posts display with fetch API and basic styling (#97)
    * Add initial implementation of posts display with fetch API and basic styling
    
    * Update Lesson23/abid/app.js
    
    Co-authored-by: Code2Career <info@annaatalay.com>
    
    * Update Lesson23/abid/app.js
    
    Co-authored-by: Code2Career <info@annaatalay.com>
    
    * Update Lesson23/abid/app.js
    
    Co-authored-by: Code2Career <info@annaatalay.com>
    
    * Update Lesson23/abid/app.js
    
    Co-authored-by: Code2Career <info@annaatalay.com>
    
    ---------
    
    Co-authored-by: Code2Career <info@annaatalay.com>

---
+++ Lesson23/abid/app.js
@@ -0,0 +1,30 @@
+
+const postsContainer = document.querySelector('.posts-container');
+
+fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Failed fetching all posts for the user. Status: ${response.status}, Status text: ${response.statusText}`);
+    }
+    console.log(response);
+    return response.json();
+  })
+
+  .then((posts) => {
+    posts.forEach(post => {
+        const card= document.createElement('div');
+        card.className = 'post-card';
+
+        const title = document.createElement('h2');
+        title.textContent = post.title;
+
+        const body = document.createElement('p');
+        body.textContent = post.body;
+
+        card.appendChild(title);
+        card.appendChild(body);
+
+        postsContainer.appendChild(card);
+    });
+    }) 
+  .catch(error => console.error('Error fetching posts:', error));
